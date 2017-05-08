bf2015bb2f41f3b431e61f9d0901bfd5e18241c4
AnthonyLam7 <anthonylam7@gmail.com>
2017-05-08 12:53:50 -0400

    Initial ROS package

---
+++ RaspberryPi/ROS_Ws/build/sunfounder_rover/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/anthony/PycharmProjects/MESA-Lab/RaspberryPi/ROS_Ws/src/sunfounder_rover
+# Build directory: /home/anthony/PycharmProjects/MESA-Lab/RaspberryPi/ROS_Ws/build/sunfounder_rover
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
